6a1857e3cced51fb064a88c1c5105f62b754ecca
R3call <thenoobyzombie@gmail.com>
2023-12-09 10:42:42 +0100

    update: added new dependency

--- src/functions/createValve.ts
+++ src/functions/createValve.ts
@@ -1,46 +1,49 @@
 import {
-	app,
-	HttpRequest,
-	HttpResponseInit,
-	InvocationContext,
+    app,
+    HttpRequest,
+    HttpResponseInit,
+    InvocationContext,
 } from "@azure/functions";
 const Registry = require("azure-iothub").Registry;
 
-// Retrieve the connection string from the local.settings.json environment variables
 const connectionString = process.env.IOTHUB_CONNECTION_STRING;
 const registry = Registry.fromConnectionString(connectionString);
 
 export async function createValve(
-	request: HttpRequest,
-	context: InvocationContext
+    request: HttpRequest,
+    context: InvocationContext
 ): Promise<HttpResponseInit> {
-	try {
-		const deviceId: string =
-			request.query.get("deviceId") || (await request.text());
-		if (!deviceId) {
-			throw new Error("DeviceId is required");
-		}
+    try {
+        const deviceId: string = request.query.get("deviceId") || (await request.text());
+        if (!deviceId) {
+            throw new Error("DeviceId is required");
+        }
 
-		context.log(`Http function processed request for url "${request.url}"`);
-		return new Promise((resolve, reject) => {
-			registry.create({ deviceId: deviceId }, (err, deviceInfo) => {
-				if (err) {
-					context.log("error: " + err.toString());
-					reject({ status: 400, body: err.toString() });
-				} else {
-					context.log("Device created: " + deviceInfo.deviceId);
-					resolve({ status: 200, body: JSON.stringify(deviceInfo) });
-				}
-			});
-		});
-	} catch (error) {
-		context.log("error: " + error.toString());
-		return { status: 500, body: error.toString() };
-	}
+        context.log(`Http function processed request for url "${request.url}"`);
+        return new Promise((resolve, reject) => {
+            registry.create({ deviceId: deviceId }, (err, deviceInfo) => {
+                if (err) {
+                    context.log("error: " + err.toString());
+                    reject({ status: 400, body: err.toString() });
+                } else {
+                    context.log("Device created: " + deviceInfo.deviceId);
+                    // Extract only the deviceId and primary key
+                    const response = {
+                        deviceId: deviceInfo.deviceId,
+                        primaryKey: deviceInfo.authentication.symmetricKey.primaryKey
+                    };
+                    resolve({ status: 200, body: JSON.stringify(response) });
+                }
+            });
+        });
+    } catch (error) {
+        context.log("error: " + error.toString());
+        return { status: 500, body: error.toString() };
+    }
 }
 
 app.http("createValve", {
-	methods: ["POST"],
-	authLevel: "anonymous",
-	handler: createValve,
+    methods: ["POST"],
+    authLevel: "anonymous",
+    handler: createValve,
 });

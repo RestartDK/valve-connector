698289bed08c1026038011196d59376b9c9b6bc9
R3call <thenoobyzombie@gmail.com>
2023-12-11 22:18:38 +0100

    updated: timezone fix

--- src/functions/scheduleValveStatus.ts
+++ src/functions/scheduleValveStatus.ts
@@ -1,39 +1,41 @@
+import { DateTime } from 'luxon';
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
 import { QueueServiceClient } from "@azure/storage-queue";
 
 export async function scheduleValveStatus(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     try {
-        const targetTime = new Date(request.query.get("targetTime")); // Target time
         const deviceId = request.query.get("deviceId"); // Device ID
         const status = request.query.get("status"); // Status ('on' or 'off')
+        const targetTimeString = request.query.get("targetTime"); // Target time as string
 
-        // Validate input
-        if (!targetTime || !deviceId || !status) {
+        if (!targetTimeString || !deviceId || !status) {
             return { status: 400, body: "Missing targetTime, deviceId, or status" };
         }
 
-        const currentTime = new Date();
-        const delayInSeconds = (targetTime.getTime() - currentTime.getTime()) / 1000;
+        // Parse the target time using Luxon
+        const targetTime = DateTime.fromISO(targetTimeString, { zone: 'local' });
+
+        if (!targetTime.isValid) {
+            return { status: 400, body: "Invalid targetTime" };
+        }
+
+        const currentTime = DateTime.local();
+        const delayInSeconds = Math.round(targetTime.diff(currentTime, 'seconds').seconds);
 
         if (delayInSeconds > 0) {
-            const queueServiceClient = QueueServiceClient.fromConnectionString(process.env.AZURE_STORAGE_CONNECTION_STRING);
+            const queueServiceClient = QueueServiceClient.fromConnectionString(process.env.AzureWebJobsStorage);
             const queueClient = queueServiceClient.getQueueClient("valvequeue");
-            
-            // Construct message content with device and status information
             const messageContent = JSON.stringify({ deviceId, status });
             await queueClient.sendMessage(Buffer.from(messageContent).toString('base64'), { visibilityTimeout: delayInSeconds });
 
-            context.log("Message sent to queue");
             return { status: 200, body: "Function scheduled" };
         } else {
-            context.log("Target time is in the past");
             return { status: 400, body: "Target time is in the past" };
         }
     } catch (error) {
-        context.log("error: " + error.toString());
         return { status: 500, body: error.toString() };
     }
-};
+}
 
 app.http('scheduleValveStatus', {
     methods: ['POST'],

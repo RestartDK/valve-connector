a808b96e6cb44ae8df4075b1467d44d524c844e6
R3call <thenoobyzombie@gmail.com>
2023-12-11 22:19:16 +0100

    added: queue functionality

--- src/functions/processValveQueue.ts
+++ src/functions/processValveQueue.ts
@@ -1,13 +1,53 @@
-import { app, InvocationContext } from "@azure/functions";
+import { app, InvocationContext, StorageQueueTrigger } from "@azure/functions";
+import { Registry } from "azure-iothub";
 
-const AZURE_STORAGE_CONNECTION_STRING = process.env.AZURE_STORAGE_CONNECTION_STRING;
+type ValveQueueMessage = {
+    deviceId: string;
+    status: string;
+};
 
-export async function processValveQueue(queueItem: unknown, context: InvocationContext): Promise<void> {
-    context.log('Storage queue function processed work item:', queueItem);
+const connectionString = process.env.IOTHUB_CONNECTION_STRING;
+const registry = Registry.fromConnectionString(connectionString);
+
+export async function processValveQueue(queueItem: ValveQueueMessage, context: InvocationContext): Promise<void> {
+    try {
+        const { deviceId, status } = queueItem;
+
+        if (!deviceId || !status) {
+            context.log("Not enough information to process queue item");
+            return; // Exit the function if essential information is missing
+        }
+
+        const desiredProperties = {
+            properties: {
+                desired: {
+                    status: status
+                }
+            }
+        };
+
+        registry.getTwin(deviceId, (err, twin) => {
+            if (err) {
+                context.error("Could not get twin: " + err.toString());
+                return; // Exit the function on error
+            }
+
+            twin.update(desiredProperties, (err) => {
+                if (err) {
+                    context.error("Could not update twin: " + err.toString());
+                    return; // Exit the function on error
+                }
+                context.log("Twin updated with desired properties");
+            });
+        });
+    } catch (error) {
+        context.log(`Error processing queue item: ${error.message}`);
+        // Handle any other unexpected errors here
+    }
 }
 
 app.storageQueue('processValveQueue', {
     queueName: 'valvequeue',
-    connection: AZURE_STORAGE_CONNECTION_STRING,
+    connection: "AzureWebJobsStorage",
     handler: processValveQueue
 });
